5100dc77b67e0c8d63ed96bb885caf9af2e1de8f
owen <owen.ottr@gmail.com>
2025-06-02 21:15:46 -0400

    idk

--- app/animations/PreLoader/loader.js
+++ app/animations/PreLoader/loader.js
@@ -1,10 +1,3 @@
-import gsap from "gsap";
-
-// Declare a general timeline to use in all the animation functions.
-
-const tl = gsap.timeline();
-
-// Preloader Animation
 export const preLoaderAnim = () => {
   tl.to(".texts-container", {
     duration: 0,
@@ -28,7 +21,7 @@ export const preLoaderAnim = () => {
     })
     .to("body", {
       duration: 0.01,
-      css: { overflowY: "scroll" },
+      css: { overflow: "auto" }, // or use clearProps: "overflow"
       ease: "power3.inOut",
     })
     .from(".sub", {
@@ -43,23 +36,12 @@ export const preLoaderAnim = () => {
         duration: 1.5,
         height: "0vh",
         ease: "Power3.easeOut",
-        onComplete: mobileLanding(),
+        onComplete: mobileLanding, // Corrected here
       },
       "-=2"
     )
     .to(".preloader", {
       duration: 0,
       css: { display: "none" },
-    });
-};
-
-export const mobileLanding = () => {
-  window.innerWidth < 763 &&
-    tl.from(".landing__main2", {
-      duration: 1,
-      delay: 0,
-      opacity: 0,
-      y: 80,
-      ease: "expo.easeOut",
     });
 };
