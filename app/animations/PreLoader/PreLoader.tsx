8c4622d5e1a28e78ade3981fc6b9347acf712c10
owen <owen.ottr@gmail.com>
2025-06-02 21:02:22 -0400

    w

--- app/animations/PreLoader/PreLoader.tsx
+++ app/animations/PreLoader/PreLoader.tsx
@@ -1,47 +1,17 @@
-import gsap from "gsap";
+"use cleint";
+import { useEffect } from "react";
+import { preLoaderAnim } from "./loader";
+import "./preloader.css";
 
-// Declare a general timeline to use in all the animation functions.
-const tl = gsap.timeline();
+const PreLoader = () => {
+  useEffect(() => {
+    preLoaderAnim();
+  }, []);
 
-// Optional intro animation
-export const preLoaderAnim = () => {
-  tl.to(".texts-container", {
-    duration: 0,
-    opacity: 1,
-    ease: "Power3.easeOut",
-  })
-    .from(".texts-container span", {
-      duration: 1.5,
-      delay: 1,
-      y: 70,
-      skewY: 10,
-      stagger: 0.4,
-      ease: "Power3.easeOut",
-    })
-    .to(".texts-container span", {
-      duration: 1,
-      y: 70,
-      skewY: -20,
-      stagger: 0.2,
-      ease: "Power3.easeOut",
-    })
-    .from(".sub", {
-      duration: 1,
-      opacity: 0,
-      y: 80,
-      ease: "expo.easeOut",
-    });
-
-  if (window.innerWidth < 763) {
-    mobileLanding();
-  }
+  return (
+    <div className="preloader">
+    </div>
+  );
 };
 
-export const mobileLanding = () => {
-  tl.from(".landing__main2", {
-    duration: 1,
-    opacity: 0,
-    y: 80,
-    ease: "expo.easeOut",
-  });
-};
+export default PreLoader;
--- app/animations/PreLoader/loader.js
+++ app/animations/PreLoader/loader.js
@@ -3,13 +3,45 @@ import gsap from "gsap";
 // Declare a general timeline to use in all the animation functions.
 const tl = gsap.timeline();
 
-export const mobileLanding = () => {
-  if (window.innerWidth < 763) {
-    tl.from(".landing__main2", {
+// Optional intro animation
+export const preLoaderAnim = () => {
+  tl.to(".texts-container", {
+    duration: 0,
+    opacity: 1,
+    ease: "Power3.easeOut",
+  })
+    .from(".texts-container span", {
+      duration: 1.5,
+      delay: 1,
+      y: 70,
+      skewY: 10,
+      stagger: 0.4,
+      ease: "Power3.easeOut",
+    })
+    .to(".texts-container span", {
+      duration: 1,
+      y: 70,
+      skewY: -20,
+      stagger: 0.2,
+      ease: "Power3.easeOut",
+    })
+    .from(".sub", {
       duration: 1,
       opacity: 0,
       y: 80,
       ease: "expo.easeOut",
     });
+
+  if (window.innerWidth < 763) {
+    mobileLanding();
   }
+};
+
+export const mobileLanding = () => {
+  tl.from(".landing__main2", {
+    duration: 1,
+    opacity: 0,
+    y: 80,
+    ease: "expo.easeOut",
+  });
 };
