f1d420c045b923e756f1e75204654f1376e3edcf
owen <owen.ottr@gmail.com>
2025-06-02 21:04:11 -0400

    1

--- app/animations/PreLoader/loader.js
+++ app/animations/PreLoader/loader.js
@@ -1,9 +1,10 @@
 import gsap from "gsap";
 
 // Declare a general timeline to use in all the animation functions.
+
 const tl = gsap.timeline();
 
-// Optional intro animation
+// Preloader Animation
 export const preLoaderAnim = () => {
   tl.to(".texts-container", {
     duration: 0,
@@ -25,23 +26,40 @@ export const preLoaderAnim = () => {
       stagger: 0.2,
       ease: "Power3.easeOut",
     })
+    .to("body", {
+      duration: 0.01,
+      css: { overflowY: "scroll" },
+      ease: "power3.inOut",
+    })
     .from(".sub", {
       duration: 1,
       opacity: 0,
       y: 80,
       ease: "expo.easeOut",
+    })
+    .to(
+      ".preloader",
+      {
+        duration: 1.5,
+        height: "0vh",
+        ease: "Power3.easeOut",
+        onComplete: mobileLanding(),
+      },
+      "-=2"
+    )
+    .to(".preloader", {
+      duration: 0,
+      css: { display: "none" },
     });
-
-  if (window.innerWidth < 763) {
-    mobileLanding();
-  }
 };
 
 export const mobileLanding = () => {
-  tl.from(".landing__main2", {
-    duration: 1,
-    opacity: 0,
-    y: 80,
-    ease: "expo.easeOut",
-  });
+  window.innerWidth < 763 &&
+    tl.from(".landing__main2", {
+      duration: 1,
+      delay: 0,
+      opacity: 0,
+      y: 80,
+      ease: "expo.easeOut",
+    });
 };
